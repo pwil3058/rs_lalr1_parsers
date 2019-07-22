adf82bb675edfbe2f030141855c203b6ea5b5505
Peter Williams <pwil3058@gmail.com>
2019-07-22 23:12:38 +1000

    Add state to next_action() arguments
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ mod tests {
 
     struct Calc {
         lexicon: lexan::Lexicon<Handle>,
+        attributes: Vec<u32>,
     }
 
     impl Calc {
@@ -46,20 +47,58 @@ mod tests {
                     r"\A([\t\r ]+)",
                 ]
             ).unwrap();
-            Self { lexicon }
+            Self { lexicon, attributes: vec![] }
         }
     }
 
-    impl parser::Parser<Handle> for Calc {
+    impl parser::Parser<Handle, u32> for Calc {
         fn lexicon(&self) -> &lexan::Lexicon<Handle> {
             &self.lexicon
         }
 
-        fn next_action(&self, o_token: Option<lexan::Token<Handle>>) -> parser::Action {
+        fn attributes(&self) -> &Vec<u32> {
+            &self.attributes
+        }
+
+        fn next_action<'a>(&self, state: u32, o_token: Option<lexan::Token<'a, Handle>>) -> parser::Action<'a> {
             if let Some(token) = o_token {
-                return parser::Action::Shift
+                use Handle::*;
+                match token {
+                    lexan::Token::UnexpectedText(text,location) => {
+                        return parser::Action::UnexpectedText(text, location);
+                    }
+                    lexan::Token::Valid(handle, text, location) => match state {
+                        0 => match handle {
+                            Minus => return parser::Action::Reduce(8),
+                            LPR => return parser::Action::Reduce(8),
+                            Number => return parser::Action::Reduce(8),
+                            Id => return parser::Action::Reduce(8),
+                            _ => return parser::Action::SyntaxError,
+                        },
+                        1 => match handle {
+                            EOL => return parser::Action::Shift(4),
+                            _ => return parser::Action::SyntaxError,
+                        },
+                        100 => match handle {
+                            Plus => return parser::Action::Shift(0),
+                            Minus => return parser::Action::Shift(0),
+                            Times => return parser::Action::Shift(0),
+                            Divide => return parser::Action::Shift(0),
+                            Assign => return parser::Action::Shift(0),
+                            LPR => return parser::Action::Shift(0),
+                            RPR => return parser::Action::Shift(0),
+                            EOL => return parser::Action::Shift(0),
+                            Number => return parser::Action::Shift(0),
+                            Id => return parser::Action::Shift(0),
+                        },
+                        _ => panic!("illegal state: {}", state)
+                    }
+                }
             } else {
-                return parser::Action::Accept
+                match state {
+                    1 => return parser::Action::Accept,
+                    _ => return parser::Action::UnexpectedEndOfInput,
+                }
             }
         }
     }
--- src/parser.rs
+++ src/parser.rs
@@ -3,26 +3,28 @@ use std::fmt::Debug;
 use lexan;
 
 #[derive(Debug, Clone, Copy)]
-pub enum Action {
-    Shift,
-    Reduce,
+pub enum Action<'a> {
+    Shift(u32),
+    Reduce(u32),
     Accept,
     SyntaxError,
-    UnexpectedText,
+    UnexpectedText(&'a str, lexan::Location<'a>),
+    UnexpectedEndOfInput,
 }
 
-pub trait Parser<H: Ord + Copy + Debug> {
+pub trait Parser<H: Ord + Copy + Debug, A> {
     fn lexicon(&self) -> &lexan::Lexicon<H>;
-
-    fn next_action(&self, o_token: Option<lexan::Token<H>>) -> Action;
+    fn attributes(&self) -> &Vec<A>;
+    fn next_action<'a>(&self, state: u32, o_token: Option<lexan::Token<'a, H>>) -> Action<'a>;
 
     fn parse_text(&self, text: &str, label: &str) -> bool {
         let mut tokens = self.lexicon().injectable_token_stream(text, label);
         let mut o_token = tokens.next();
+        let mut state: u32 = 0;
         loop {
-            match self.next_action(o_token) {
-                Action::Shift => println!("shift"),
-                Action::Reduce => println!("reduce"),
+            match self.next_action(state, o_token) {
+                Action::Shift(state) => println!("shift: {}", state),
+                Action::Reduce(production) => println!("reduce: {}", production),
                 Action::Accept => break,
                 _ => return false,
             };
