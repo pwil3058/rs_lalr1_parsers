ff640e3ef03cf7d6a95fa6210f4bcc7359503206
Peter Williams <pwil3058@gmail.com>
2019-07-29 16:41:58 +1000

    Redesign Parser to do more of the grunt work
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,8 @@
-use std::default::Default;
-use std::fmt::{Debug, Display};
+use std::{
+    convert::From,
+    default::Default,
+    fmt::{Debug, Display},
+};
 
 use lexan;
 
@@ -20,6 +23,56 @@ pub enum Symbol<T, N> {
     Invalid,
 }
 
+#[derive(Debug)]
+pub struct ParseStack<T, N, A>
+where
+    T: Copy + Ord + Debug,
+    A: From<(T, String)>,
+{
+    states: Vec<(Symbol<T, N>, u32)>,
+    attributes: Vec<A>,
+}
+
+impl<T, N, A> ParseStack<T, N, A>
+where
+    T: Copy + Ord + Debug,
+    A: From<(T, String)>,
+{
+    fn new() -> Self {
+        Self {
+            states: vec![(Symbol::Start, 0)],
+            attributes: vec![],
+        }
+    }
+
+    fn current_state(&self) -> u32 {
+        self.states.last().unwrap().1
+    }
+
+    pub fn attribute_n_from_end<'a>(&'a self, n: usize) -> &'a A {
+        let len = self.attributes.len();
+        &self.attributes[len - n]
+    }
+
+    fn pop_n(&mut self, n: usize) -> Vec<A> {
+        let len = self.states.len();
+        self.states.truncate(len - n);
+        let len = self.attributes.len();
+        self.attributes.split_off(len - n)
+    }
+
+    fn push_terminal(&mut self, terminal: T, string: &str, new_state: u32) {
+        let attribute = A::from((terminal, string.into()));
+        self.attributes.push(attribute);
+        self.states.push((Symbol::Terminal(terminal), new_state));
+    }
+
+    fn push_non_terminal(&mut self, non_terminal: N, new_state: u32) {
+        self.states
+            .push((Symbol::NonTerminal(non_terminal), new_state));
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum Action {
     Shift(u32),
@@ -33,11 +86,11 @@ pub enum Coda {
     UnexpectedEndOfInput,
 }
 
-pub trait Parser<T: Ord + Copy + Debug, N, A>
+pub trait Parser<'t, T: Ord + Copy + Debug, N, A>
 where
     T: Ord + Copy + Debug,
     N: Ord + Display + Debug,
-    A: Default,
+    A: Default + From<(T, String)>,
 {
     fn lexical_analyzer(&self) -> &lexan::LexicalAnalyzer<T>;
     fn attribute<'b>(&'b self, attr_num: usize, num_attrs: usize) -> &'b A;
@@ -49,10 +102,11 @@ where
     fn next_action<'a>(
         &self,
         state: u32,
+        attributes: &ParseStack<T, N, A>,
         o_token: &lexan::Token<'a, T>,
     ) -> Result<Action, Error<'a, T>>;
-    fn next_coda<'a>(&self, state: u32) -> Coda;
-    fn production_data(&mut self, production_id: u32) -> (N, Vec<A>);
+    fn next_coda<'a>(&self, state: u32, attributes: &ParseStack<T, N, A>) -> Coda;
+    fn production_data(&mut self, production_id: u32) -> (N, usize);
     fn goto_state(lhs: &N, current_state: u32) -> u32;
 
     fn report_error(error: &Error<T>) {
@@ -72,7 +126,7 @@ where
 
     fn parse_text<'a>(&mut self, text: &'a str, label: &'a str) -> Result<(), Error<'a, T>> {
         let mut tokens = self.lexical_analyzer().injectable_token_stream(text, label);
-        self.push_state(0, Symbol::Start);
+        let mut parse_stack = ParseStack::<T, N, A>::new();
         let mut result: Result<(), Error<'a, T>> = Ok(());
 
         let mut o_r_token = tokens.next();
@@ -86,41 +140,44 @@ where
                         return result;
                     }
                 }
-                Ok(token) => match self.next_action(self.current_state(), &token) {
-                    Ok(action) => match action {
-                        Action::Shift(state) => {
-                            self.push_state(state, Symbol::Terminal(*token.tag()));
-                            self.push_attribute(A::default());
-                            o_r_token = tokens.next();
-                        }
-                        Action::Reduce(production_id) => {
-                            let (lhs, rhs) = self.production_data(production_id);
-                            self.pop_states(rhs.len());
-                            let next_state = Self::goto_state(&lhs, self.current_state());
-                            self.push_state(next_state, Symbol::NonTerminal(lhs));
-                            self.push_attribute(A::default());
-                        }
-                    },
-                    Err(err) => {
-                        Self::report_error(&err);
-                        result = Err(err);
-                        if Self::short_circuit() {
-                            return result;
+                Ok(token) => {
+                    match self.next_action(parse_stack.current_state(), &parse_stack, &token) {
+                        Ok(action) => match action {
+                            Action::Shift(next_state) => {
+                                parse_stack.push_terminal(*token.tag(), token.lexeme(), next_state);
+                                o_r_token = tokens.next();
+                            }
+                            Action::Reduce(production_id) => {
+                                let (lhs, rhs_len) = self.production_data(production_id);
+                                let _rhs = parse_stack.pop_n(rhs_len);
+                                let next_state =
+                                    Self::goto_state(&lhs, parse_stack.current_state());
+                                parse_stack.push_non_terminal(lhs, next_state);
+                                self.push_attribute(A::default());
+                                // TODO: do semantic actions here
+                            }
+                        },
+                        Err(err) => {
+                            Self::report_error(&err);
+                            result = Err(err);
+                            if Self::short_circuit() {
+                                return result;
+                            }
                         }
                     }
-                },
+                }
             };
         }
-        let mut coda = self.next_coda(self.current_state());
-        let mut x = 0;
+        let mut coda = self.next_coda(parse_stack.current_state(), &parse_stack);
         while let Coda::Reduce(production_id) = coda {
-            let (lhs, rhs) = self.production_data(production_id);
-            self.pop_states(rhs.len());
-            let next_state = Self::goto_state(&lhs, self.current_state());
-            self.push_state(next_state, Symbol::NonTerminal(lhs));
+            let (lhs, rhs_len) = self.production_data(production_id);
+            let _rhs = parse_stack.pop_n(rhs_len);
+            let next_state = Self::goto_state(&lhs, parse_stack.current_state());
+            parse_stack.push_non_terminal(lhs, next_state);
             self.push_attribute(A::default());
+            // TODO: do semantic actions here
 
-            coda = self.next_coda(self.current_state());
+            coda = self.next_coda(parse_stack.current_state(), &parse_stack);
         }
         if let Coda::UnexpectedEndOfInput = coda {
             let err = Error::UnexpectedEndOfInput;
