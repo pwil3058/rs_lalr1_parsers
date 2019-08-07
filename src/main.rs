1ca34513015f449cfaedec7b1419b5bac6ff2028
Peter Williams <pwil3058@gmail.com>
2019-08-07 16:25:14 +1000

    Start testing using a simple spec file
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate clap;
+
+use std::fs;
+use std::io::prelude::*;
+
+use lalr1plus::parser::*;
 
 mod attributes;
 mod bootstrap;
@@ -7,6 +13,14 @@ mod grammar;
 mod symbols;
 
 fn main() {
-    let _parser_specification = grammar::ParserSpecification::new();
+    let matches = clap::App::new("Augmented Lexical Analyzer and Parser Generator")
+        .arg(clap::Arg::with_name("input").required(true))
+        .get_matches();
+    let file_name = matches.value_of("input").expect("\"input\" is a required argument");
+    let mut file = fs::File::open(file_name).unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
+    let mut parser_specification = grammar::ParserSpecification::new();
+    parser_specification.parse_text(input, file_name.to_string());
     println!("Hello, world!");
 }
