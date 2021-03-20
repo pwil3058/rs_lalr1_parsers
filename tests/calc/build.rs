c1c4fe2ebdaac8babaac845be86e94ce932c938a
Peter Williams <pwil3058@gmail.com>
2021-03-21 09:20:04 +1000

    Move calc out of the way of the main build and automate creation of calc.rs
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

---
+++ tests/calc/build.rs
@@ -0,0 +1,11 @@
+// Copyright 2021 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
+use std::process::Command;
+
+fn main() {
+    println!("cargo:rerun-if-changed=src/calc.alaps");
+    Command::new("../../target/debug/alap_gen")
+        .args(&["-f", "-e1", "src/calc.alaps"])
+        .status()
+        .unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+}
