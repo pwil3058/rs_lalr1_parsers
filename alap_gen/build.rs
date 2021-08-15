118226bec87dd4d32427403db92234471a0a7f2c
Peter Williams <pwil3058@bigpond.net.au>
2021-08-15 11:15:14 +1000

    alap_gen::build: make sure it builds out of the box

--- alap_gen/build.rs
+++ alap_gen/build.rs
@@ -1,27 +1,32 @@
 // Copyright 2021 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
-use std::process::Command;
+#[cfg(not(feature = "bootstrap"))]
+use std::{process::Command, path::Path};
 
 #[cfg(not(feature = "bootstrap"))]
 fn main() {
     println!("cargo:rerun-if-changed=src/alap_gen.alaps");
-    println!("cargo:rerun-if-changed=../target/debug/alap_gen");
-    match Command::new("../target/debug/alap_gen")
-        .args(&["-f", "src/alap_gen.alaps"])
-        .status()
-    {
-        Ok(status) => {
-            if status.success() {
-                Command::new("rustfmt")
-                    .args(&["src/alap_gen.rs"])
-                    .status()
-                    .unwrap();
-            } else {
-                panic!("failed prebuild: {}", status);
-            };
+    let alap_gen_path = "../target/debug/alap_gen";
+    if Path::new(alap_gen_path).exists() {
+        println!("cargo:rerun-if-changed=../target/debug/alap_gen");
+        println!("cargo::rerun-if-changed={}", alap_gen_path);
+        match Command::new(alap_gen_path)
+            .args(&["-f", "src/alap_gen.alaps"])
+            .status()
+        {
+            Ok(status) => {
+                if status.success() {
+                    Command::new("rustfmt")
+                        .args(&["src/alap_gen.rs"])
+                        .status()
+                        .unwrap();
+                } else {
+                    panic!("failed prebuild: {}", status);
+                };
+            }
+            Err(err) => panic!("Build error: {}", err),
         }
-        Err(err) => panic!("Build error: {}", err),
+        println!("cargo:rerun-if-changed=buildx");
     }
-    println!("cargo:rerun-if-changed=buildx");
 }
 
 #[cfg(feature = "bootstrap")]
