a3a827c1fc25868bdef8ea92671df633d5e91945
Peter Williams <pwil3058@bigpond.net.au>
2023-02-02 15:43:02 +1000

    Implement Error for LexanError

--- lexan/src/error.rs
+++ lexan/src/error.rs
@@ -1,4 +1,6 @@
 use std::convert::From;
+use std::fmt::Formatter;
+use std::{error, fmt};
 
 #[derive(Debug, PartialEq)]
 pub enum LexanError<'a, T> {
@@ -13,3 +15,16 @@ impl<'a, T> From<regex::Error> for LexanError<'a, T> {
         LexanError::RegexError(error)
     }
 }
+
+impl<'a, T: fmt::Debug> fmt::Display for LexanError<'a, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::DuplicateHandle(t) => write!(f, "{t:?}: Duplicate handle"),
+            Self::DuplicatePattern(s) => write!(f, "{s:?}: Duplicate pattern"),
+            Self::EmptyPattern(p) => write!(f, "{p:?}: Empty pattern"),
+            Self::RegexError(re) => write!(f, "Regex Error: {re:?}"),
+        }
+    }
+}
+
+impl<'a, T: fmt::Debug> error::Error for LexanError<'a, T> {}
