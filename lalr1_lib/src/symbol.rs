d63ce2e6e20599979f25a81f8cabc3e0b22bb54d
Peter Williams <pwil3058@bigpond.net.au>
2026-08-17 17:17:09 +1000

    lalr1_lib: update copyright

--- lalr1_lib/src/symbol.rs
+++ lalr1_lib/src/symbol.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
+// Copyright (c) 2026 Peter Williams <pwil3058@bigpond.net.au> <pwil3058@gmail.com>.
 use std::{collections::BTreeMap, fmt};
 
 use crate::symbol::non_terminal::NonTerminal;
