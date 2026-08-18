fd2066a80595162fdbf0c0aac48e429114d5a8b6
Peter Williams <pwil3058@bigpond.net.au>
2026-08-19 07:27:02 +1000

    Add TryFrom<Path> for ParserGenerator

--- lalr1_lib/src/lib.rs
+++ lalr1_lib/src/lib.rs
@@ -9,6 +9,7 @@ mod state;
 mod symbol;
 
 use std::io;
+use std::io::Read;
 use std::path::Path;
 use thiserror::Error;
 
@@ -22,6 +23,8 @@ pub enum Error {
     SpecificationError(#[from] lalr1::Error<AATerminal>),
     #[error("Grammar error {0}")]
     GrammarError(#[from] grammar::Error),
+    #[error("I/O error {0}")]
+    IoError(#[from] io::Error),
 }
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -37,5 +40,16 @@ impl ParserGenerator {
     pub fn write_parser_code_to_file(&self, output_path: impl AsRef<Path>) -> io::Result<()> {
         let output_path = output_path.as_ref();
         self.0.write_parser_code_to_file(output_path)
+    }
+}
+
+impl TryFrom<&Path> for ParserGenerator {
+    type Error = Error;
+
+    fn try_from(path: &Path) -> Result<ParserGenerator> {
+        let mut file = std::fs::File::open(&path)?;
+        let mut specification_text = String::new();
+        file.read_to_string(&mut specification_text)?;
+        ParserGenerator::new(specification_text.trim(), path.to_str().unwrap())
     }
 }
