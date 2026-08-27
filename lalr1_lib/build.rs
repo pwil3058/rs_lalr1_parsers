27a00832f5825dc25bc7cf0d711fc2f14db03957
Peter Williams <pwil3058@bigpond.net.au>
2026-08-27 12:51:02 +1000

    lalr1_lib/gen: move attribute code into bootstrap/parser

--- lalr1_lib/build.rs
+++ lalr1_lib/build.rs
@@ -4,8 +4,6 @@ use std::{path::Path, process::Command};
 fn main() {
     let lalr1_gen_path = "../target/debug/lalr1_gen";
     if Path::new(lalr1_gen_path).exists() {
-        // println!("cargo::rerun-if-changed=src/parser.laps");
-        // println!("cargo::rerun-if-changed={lalr1_gen_path}");
         match Command::new(lalr1_gen_path)
             .args(["-f", "src/parser.laps"])
             .status()
@@ -13,11 +11,11 @@ fn main() {
             Ok(status) => {
                 if status.success() {
                     Command::new("rustfmt")
-                        .args(["src/parser"])
+                        .args(["src/parser.rs"])
                         .status()
                         .unwrap();
                 } else {
-                    panic!("failed prebuild: {status}");
+                    panic!("failed prebuild formatting: {status}");
                 };
             }
             Err(err) => panic!("Build error: {err}"),
